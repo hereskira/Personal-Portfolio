9adb0582f325f0a80eebf8b5dd214e2616875a38
waystarco <shakiraregalado@gmail.com>
2024-02-15 18:31:18 +0800

    index.html js

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+document.querySelector('.button1').addEventListener('click', function () {
+    document.querySelector('.aboutme-pic').style.display = 'block';
+    document.querySelector('.aboutme-pic2').style.display = 'none';
+
+    document.querySelector('.perso-name').style.display = 'block';
+    document.querySelector('.perso-name2').style.display = 'none';
+
+    document.querySelector('.button1').classList.add('active');
+    document.querySelector('.button2').classList.remove('active');
+});
+
+document.querySelector('.button2').addEventListener('click', function () {
+    document.querySelector('.aboutme-pic').style.display = 'none';
+    document.querySelector('.aboutme-pic2').style.display = 'block';
+
+    document.querySelector('.perso-name').style.display = 'none';
+    document.querySelector('.perso-name2').style.display = 'block';
+
+    document.querySelector('.button2').classList.add('active');
+    document.querySelector('.button1').classList.remove('active');
+});
